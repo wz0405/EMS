3f8e89ae55ea6ded9d7b417d4017ccfac2e1b6a9
hhhan <hhhan@wiezon.com>
2020-11-04 18:54:12 +0900

    json to map

--- handler/apiHandler.go
+++ handler/apiHandler.go
@@ -9,11 +9,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Board struct {
+	ID      string
+	Title   string
+	Content string
+}
+
 func APIBlogSearchHandler(c *gin.Context) {
 	//c.Header("Content-Type", "application/json charset=utf-8")
 
 	//TODO: 해당주소에 API호출 프로토콜 가져와보기
-	resp, err := http.Get("http://106.254.240.205:3030/posts/")
+	resp, err := http.Get("http://106.254.240.205:3030/posts/1")
 
 	if err != nil {
 		panic(err)
@@ -21,11 +27,17 @@ func APIBlogSearchHandler(c *gin.Context) {
 	defer resp.Body.Close()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
+
+	var data map[string]interface{}
+
+	json.Unmarshal([]byte(respBody), &data)
+	// fmt.Println(bo.ID, bo.Title, bo.Content)
+
 	if err == nil {
 		str := string(respBody)
 		//println(str)
 		fmt.Printf("%s\n", str)
 	}
-	b, _ := json.Marshal(str)
-	fmt.Println(b)
+
+	fmt.Println(data["id"], data["title"])
 }
